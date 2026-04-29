bac8e672b3dbdb3baa6bbf2e10a21a673e6fe775
saanviaesthetics2013-coder <saanviaesthetics2013@gmail.com>
2026-04-29 19:39:01 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,27 @@
+const posts = [
+  {
+    slug: "my-first-blog",
+    title: "My First Blog Post",
+    date: "2026-01-10",
+    content: `
+      <p>This is my first blog post on my new portfolio website.</p>
+      <p>I built this website using <b>HTML, CSS, and JavaScript</b>.</p>
+      <p>More posts coming soon 🚀</p>
+    `
+  },
+  {
+    slug: "how-i-learned-javascript",
+    title: "How I Learned JavaScript",
+    date: "2026-02-02",
+    content: `
+      <p>JavaScript is one of the most powerful languages for web development.</p>
+      <p>I learned it by building projects every day.</p>
+      <ul>
+        <li>Start with basics</li>
+        <li>Build small apps</li>
+        <li>Practice daily</li>
+      </ul>
+      <p>Consistency is everything.</p>
+    `
+  }
+];
