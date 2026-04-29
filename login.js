a6186b9578a2f907e58ede7e73a358aae5fcef3f
saanviaesthetics2013-coder <saanviaesthetics2013@gmail.com>
2026-04-29 19:31:37 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("loginForm");
+const errorMsg = document.getElementById("errorMsg");
+
+// CHANGE THIS LOGIN DETAILS
+const correctUsername = "admin";
+const correctPassword = "1234";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username === correctUsername && password === correctPassword) {
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "index.html";
+  } else {
+    errorMsg.textContent = "❌ Wrong username or password!";
+  }
+});
